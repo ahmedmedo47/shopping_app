f69e84592c8ed8d933ddee9404cb63f8aaf32864
Ahmed-Waheed <ahm761171@gmail.com>
2024-07-22 15:31:52 +0300

    Add product and category models

---
+++ lib/features/home/data/models/products/category.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+
+class Category {
+  int? id;
+  String? name;
+  String? image;
+  DateTime? creationAt;
+  DateTime? updatedAt;
+
+  Category({
+    this.id,
+    this.name,
+    this.image,
+    this.creationAt,
+    this.updatedAt,
+  });
+
+  factory Category.fromMap(Map<String, dynamic> data) => Category(
+        id: data['id'] as int?,
+        name: data['name'] as String?,
+        image: data['image'] as String?,
+        creationAt: data['creationAt'] == null
+            ? null
+            : DateTime.parse(data['creationAt'] as String),
+        updatedAt: data['updatedAt'] == null
+            ? null
+            : DateTime.parse(data['updatedAt'] as String),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'name': name,
+        'image': image,
+        'creationAt': creationAt?.toIso8601String(),
+        'updatedAt': updatedAt?.toIso8601String(),
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [Category].
+  factory Category.fromJson(String data) {
+    return Category.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [Category] to a JSON string.
+  String toJson() => json.encode(toMap());
+}
---
+++ lib/features/home/data/models/products/products.dart
@@ -0,0 +1,60 @@
+import 'dart:convert';
+import 'category.dart';
+
+class Products {
+  int? id;
+  String? title;
+  int? price;
+  String? description;
+  List<String>? images;
+  DateTime? creationAt;
+  DateTime? updatedAt;
+  Category? category;
+
+  Products({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.images,
+    this.creationAt,
+    this.updatedAt,
+    this.category,
+  });
+
+  factory Products.fromMap(Map<String, dynamic> data) => Products(
+        id: data['id'] as int?,
+        title: data['title'] as String?,
+        price: data['price'] as int?,
+        description: data['description'] as String?,
+        images: data['images'] as List<String>?,
+        creationAt: data['creationAt'] == null
+            ? null
+            : DateTime.parse(data['creationAt'] as String),
+        updatedAt: data['updatedAt'] == null
+            ? null
+            : DateTime.parse(data['updatedAt'] as String),
+        category: data['category'] == null
+            ? null
+            : Category.fromMap(data['category'] as Map<String, dynamic>),
+      );
+
+  Map<String, dynamic> toMap() => {
+        'id': id,
+        'title': title,
+        'price': price,
+        'description': description,
+        'images': images,
+        'creationAt': creationAt?.toIso8601String(),
+        'updatedAt': updatedAt?.toIso8601String(),
+        'category': category?.toMap(),
+      };
+
+
+  factory Products.fromJson(String data) {
+    return Products.fromMap(json.decode(data) as Map<String, dynamic>);
+  }
+
+
+  String toJson() => json.encode(toMap());
+}
