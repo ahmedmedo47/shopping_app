cc8f76005c14b82d18c5a66e2041879dcddcab55
Ahmed-Waheed <ahm761171@gmail.com>
2024-07-22 22:31:06 +0300

    Fix import and formatting issues in home view product details widget and main.dart, and remove smooth_page_indicator dependency from pubspec.yaml.

--- lib/features/home/presentation/views/widgets/home_view_product_details/home_view_product_details_widget.dart
+++ lib/features/home/presentation/views/widgets/home_view_product_details/home_view_product_details_widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:shopping_app/features/home/presentation/views/widgets/home_view_product_details/custom_appbar_for_details_screen.dart';
 import 'package:shopping_app/features/home/presentation/views/widgets/home_view_product_details/product_details_tile.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeViewProductDetailsWidget extends StatefulWidget {
-  HomeViewProductDetailsWidget({Key? key}) : super(key: key);
+  const HomeViewProductDetailsWidget({super.key});
 
   @override
-  State<HomeViewProductDetailsWidget> createState() => _HomeViewProductDetailsWidgetState();
-  bool addedToTheCart = false;
+  State<HomeViewProductDetailsWidget> createState() =>
+      _HomeViewProductDetailsWidgetState();
 }
 
-class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWidget> {
+class _HomeViewProductDetailsWidgetState
+    extends State<HomeViewProductDetailsWidget> {
   final PageController _pageController = PageController();
+  bool addedToTheCart = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +31,14 @@ class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWid
               child: PageView(
                 controller: _pageController,
                 children: [
-                  Image.asset('assets/images/black-smooth-textured-paper-background.jpg', fit: BoxFit.fill),
-                  Image.asset('assets/images/download (1).png', fit: BoxFit.fill),
-                  Image.asset('assets/images/HD-wallpaper-patchwork-red-screen-usa.jpg', fit: BoxFit.fill),
+                  Image.asset(
+                      'assets/images/black-smooth-textured-paper-background.jpg',
+                      fit: BoxFit.fill),
+                  Image.asset('assets/images/download (1).png',
+                      fit: BoxFit.fill),
+                  Image.asset(
+                      'assets/images/HD-wallpaper-patchwork-red-screen-usa.jpg',
+                      fit: BoxFit.fill),
                 ],
               ),
             ),
@@ -42,8 +49,8 @@ class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWid
               child: Center(
                 child: SmoothPageIndicator(
                   controller: _pageController,
-                  count: 3,  // Number of pages
-                  effect: ExpandingDotsEffect(
+                  count: 3, // Number of pages
+                  effect: const ExpandingDotsEffect(
                     dotWidth: 10,
                     dotHeight: 10,
                     spacing: 16,
@@ -54,21 +61,22 @@ class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWid
                 ),
               ),
             ),
-            CustomAppbarForDetailsScreen(),
+            const CustomAppbarForDetailsScreen(),
           ],
         ),
         Expanded(
           child: Container(
             width: width,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Colors.black,
             ),
             child: Container(
               width: width,
-              decoration: BoxDecoration(
+              decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(15), topRight: Radius.circular(15))),
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15))),
               child: Padding(
                 padding: const EdgeInsets.all(15.0),
                 child: SingleChildScrollView(
@@ -76,50 +84,61 @@ class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWid
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(
+                      const Text(
                         "120,00 Є",
                         style: TextStyle(color: Colors.black, fontSize: 20),
                       ),
-                      Text(
+                      const Text(
                         "Details",
                         style: TextStyle(color: Colors.black38, fontSize: 14),
                       ),
-                      ProductDetailsTile(title: 'title', description: 'description'),
-                      ProductDetailsTile(title: 'title', description: 'description'),
-                      ProductDetailsTile(title: 'title', description: 'description'),
-                      SizedBox(height: 20),
+                      const ProductDetailsTile(
+                          title: 'title', description: 'description'),
+                      const ProductDetailsTile(
+                          title: 'title', description: 'description'),
+                      const ProductDetailsTile(
+                          title: 'title', description: 'description'),
+                      const SizedBox(height: 20),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           MaterialButton(
-                            onPressed: (){
+                            onPressed: () {
                               _messageAlret("title", "text");
                             },
                             child: Container(
                               width: 240,
                               height: 40,
-                              decoration: BoxDecoration(
+                              decoration: const BoxDecoration(
                                 color: Colors.green,
-                                borderRadius: BorderRadius.all(Radius.circular(20)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
                               ),
-                              child: Center(child: Text("+ Add To Cart",style: TextStyle(fontSize: 20,color: Colors.white),)),
+                              child: const Center(
+                                  child: Text(
+                                "+ Add To Cart",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.white),
+                              )),
                             ),
                           ),
                           Container(
                             width: 70,
                             height: 40,
                             decoration: BoxDecoration(
-                                color: Color(0xffecf0ec),
-                                borderRadius: BorderRadius.circular(10)
-                            ),
+                                color: const Color(0xffecf0ec),
+                                borderRadius: BorderRadius.circular(10)),
                             child: IconButton(
                               icon: Icon(
-                                widget.addedToTheCart ? Icons.favorite : Icons.favorite_border,
-                                color: widget.addedToTheCart ? Colors.red : Colors.black,
+                                addedToTheCart
+                                    ? Icons.favorite
+                                    : Icons.favorite_border,
+                                color:
+                                    addedToTheCart ? Colors.red : Colors.black,
                               ),
-                              onPressed: (){
+                              onPressed: () {
                                 setState(() {
-                                  widget.addedToTheCart = !widget.addedToTheCart;
+                                  addedToTheCart = addedToTheCart;
                                 });
                               },
                             ),
@@ -142,14 +161,14 @@ class _HomeViewProductDetailsWidgetState extends State<HomeViewProductDetailsWid
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text(title, style: TextStyle(fontSize: 30)),
-          content: Text(text, style: TextStyle(fontSize: 20)),
+          title: Text(title, style: const TextStyle(fontSize: 30)),
+          content: Text(text, style: const TextStyle(fontSize: 20)),
           actions: [
             TextButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Close'),
+              child: const Text('Close'),
             ),
           ],
         );
