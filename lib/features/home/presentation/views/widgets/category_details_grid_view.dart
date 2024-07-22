ecd8552a95f14ec7a7e6489278673bd3ddbcc440
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-07-23 02:33:58 +0300

    add Category Details view

---
+++ lib/features/home/presentation/views/widgets/category_details_grid_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/features/home/presentation/views/widgets/category_details.dart';
+
+class CategoryDetailsGridView extends StatelessWidget {
+  const CategoryDetailsGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: 20,
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        childAspectRatio: 0.56,
+      ),
+      itemBuilder: (context, i) {
+        return CategoryDetails();
+      },
+      shrinkWrap: true, // هذه الخاصية تجعل GridView يضبط حجمه بناءً على محتوياته
+      physics: NeverScrollableScrollPhysics(), // هذا يمنع GridView من التمرير نفسه
+    );
+  }
+}
