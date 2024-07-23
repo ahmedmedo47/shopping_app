eb040241822951519bf4f46fd18ca3e92c29877e
AhmedYasserww <ahmedyasser2105ww@gmail.com>
2024-07-23 23:05:11 +0300

    add Favorite screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_app/features/home/presentation/views/favorite_view.dart';
 import 'package:shopping_app/features/home/presentation/views/home_view_category_details.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
        // scaffoldBackgroundColor: Color(0xfffffcfc),
 
       ),
-      home: HomeViewCategoryDetails()
+      home: FavoriteView()
 
     );
   }
