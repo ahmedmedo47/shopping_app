52f728bc7a183e4be376a2dd8e396c0768664d54
Ahmed-Waheed <ahm761171@gmail.com>
2024-07-23 15:04:08 +0300

    Refactor HomeViewProductDetailsScreen constructor signature

--- lib/features/home/presentation/views/home_view_product_details_Screen.dart
+++ lib/features/home/presentation/views/home_view_product_details_Screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/features/home/presentation/views/widgets/home_view_product_details/home_view_product_details_widget.dart';
 class HomeViewProductDetailsScreen extends StatelessWidget {
-  const HomeViewProductDetailsScreen({Key? key}) : super(key: key);
+  const HomeViewProductDetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
