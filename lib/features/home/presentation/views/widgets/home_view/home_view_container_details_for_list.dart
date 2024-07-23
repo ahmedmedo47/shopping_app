d9b717ea6e8379c59a6248b8451fbeb1003ce168
DELL <omarmohamed01284@gmail.com>
2024-07-23 18:54:18 +0300

    few changes

--- lib/features/home/presentation/views/widgets/home_view/home_view_container_details_for_list.dart
+++ lib/features/home/presentation/views/widgets/home_view/home_view_container_details_for_list.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
-class HomeViewContainerDetailsForList extends StatelessWidget {
-  const HomeViewContainerDetailsForList({Key? key, required this.image, required this.price}) : super(key: key);
+class HomeViewContainerDetailsForList extends StatefulWidget {
+   HomeViewContainerDetailsForList({super.key, required this.image, required this.price});
   final String image;
   final String price;
+  bool addedToTheCart = false;
 
+  @override
+  State<HomeViewContainerDetailsForList> createState() => _HomeViewContainerDetailsForListState();
+}
+
+class _HomeViewContainerDetailsForListState extends State<HomeViewContainerDetailsForList> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
@@ -13,38 +19,68 @@ class HomeViewContainerDetailsForList extends StatelessWidget {
         InkWell(
           onTap: (){
             Navigator.pushNamed(context, "HomeViewProductDetailsScreen",arguments: {
-              "image" : image,
-              "price" : price,
+              "image" : widget.image,
+              "price" : widget.price,
             });
           },
-          child: Container(
-            width: width * 0.6,
-            height: height * 0.5,
-            decoration: BoxDecoration(
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(15)
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: height*.35,
+          child: Stack(
+            children: [
+              Container(
+                width: width * 0.6,
+                height: height * 0.5,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: height*.35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image:  DecorationImage(image: AssetImage(widget.image),fit: BoxFit.fill),
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                     Padding(
+                      padding:  const EdgeInsets.only(left: 15.0),
+                      child:  Text(widget.price,style: const TextStyle(fontSize: 20,color: Colors.black),),
+                    ),
+                    const Padding(
+                      padding:  EdgeInsets.only(left: 15.0),
+                      child:  Text("Noisy May short oversized sweatshirt in gray",style: TextStyle(fontSize: 15,color: Colors.black26),),
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: height * .15,
+                right: 2,
+                child: Container(
+                  width: 40,
+                  height: 40,
                   decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image:  DecorationImage(image: AssetImage("$image"),fit: BoxFit.fill),
+                    color: const Color(0xffecf0ec),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.addedToTheCart = !widget.addedToTheCart;
+                      });
+                    },
+                    icon: Icon(
+                      widget.addedToTheCart
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      color:
+                      widget.addedToTheCart ? Colors.red : Colors.black,
+                    ),
                   ),
                 ),
-                const SizedBox(height: 10,),
-                 Padding(
-                  padding:  EdgeInsets.only(left: 20.0),
-                  child:  Text("$price",style: TextStyle(fontSize: 20,color: Colors.black),),
-                ),
-                const Padding(
-                  padding:  EdgeInsets.only(left: 20.0),
-                  child:  Text("Noisy May short oversized sweatshirt in gray",style: TextStyle(fontSize: 15,color: Colors.black26),),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
         ),
         const SizedBox(width: 20,)
