e4d69b341a4e9f65ab77ff2726e3983e31a5379c
DELL <omarmohamed01284@gmail.com>
2024-07-26 21:29:28 +0300

    make all api fetch one time

--- lib/features/home/manager/fetch_one_product/fetch_one_product_cubit.dart
+++ lib/features/home/manager/fetch_one_product/fetch_one_product_cubit.dart
@@ -1,30 +1,36 @@
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
+import 'package:shopping_app/core/errors/faliure.dart';
 import 'package:shopping_app/features/home/data/models/product_model_and_his_variants/product_model_and_his_variants.dart';
 import 'package:shopping_app/features/home/data/repo/home_repo.dart';
-
-import '../../../../core/errors/faliure.dart';
-import '../../data/models/product_model_and_his_variants/variant.dart';
 
 part 'fetch_one_product_state.dart';
 
 class FetchOneProductCubit extends Cubit<FetchOneProductState> {
-  FetchOneProductCubit(this.homeRepo) : super(FetchOneProductInitial());
   final HomeRepo homeRepo;
+  final Map<int, ProductModelAndHisVariants> _cachedProducts = {};
+
+  FetchOneProductCubit(this.homeRepo) : super(FetchOneProductInitial());
 
   Future<void> fetchOneProducts({required int id}) async {
+    if (_cachedProducts.containsKey(id)) {
+      emit(FetchOneProductsSuccess(productsList: _cachedProducts[id]!));
+      return;
+    }
+
     emit(FetchOneProductsLoading());
     final Either<Failure, ProductModelAndHisVariants> data = await homeRepo.fetchOneProduct(id: id);
 
     data.fold(
           (failure) {
-        print('Fetch failed: ${failure.errorMessage}');
+        print('Fetch failed: ${failure.toString()}');
         emit(FetchOneProductsFailure(failure.toString()));
       },
           (productModel) {
         print('Fetch successful');
-        emit(FetchOneProductsSuccess(productsList:productModel ));
+        _cachedProducts[id] = productModel;
+        emit(FetchOneProductsSuccess(productsList: productModel));
       },
     );
   }
