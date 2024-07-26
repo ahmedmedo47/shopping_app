0b2c9f4e4cb28a67088edb4a91f6d6d4c74c5a95
Ahmed-Waheed <ahm761171@gmail.com>
2024-07-26 18:51:32 +0300

    Add logging to FavoritesProvider
    Added logging to FavoritesProvider to track when a product is added or removed from favorites. Also made FavoriteViewBody a const widget.

--- lib/features/home/manager/product_provider.dart
+++ lib/features/home/manager/product_provider.dart
@@ -1,18 +1,20 @@
+import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/features/home/data/models/product_model_and_his_variants/variant.dart';
 
 class FavoritesProvider extends ChangeNotifier {
-  List<Variant> _favorites = [];
-
+  final List<Variant> _favorites = [];
   List<Variant> get favorites => _favorites;
 
   void addFavorite(Variant product) {
     _favorites.add(product);
+    log("added product is : $product");
     notifyListeners();
   }
 
   void removeFavorite(Variant product) {
     _favorites.remove(product);
+    log("removed product is : $product");
     notifyListeners();
   }
 }
