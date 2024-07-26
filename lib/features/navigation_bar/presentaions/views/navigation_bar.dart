2d523b4da77ebcb22751c35ea4791eaa44fef1e1
Ahmed-Waheed <ahm761171@gmail.com>
2024-07-26 23:50:31 +0300

    Refactor NavigationBarForApp to preserve state across tabs using IndexedStack
    - Updated NavigationBarForApp to use IndexedStack to avoid unnecessary re-fetching of data and maintain state.
    - This change ensures that HomeView, SearchView, CartView, and FavoriteView do not lose their state when navigating between tabs.
    - Improved the user experience by maintaining the state of each tab.

--- lib/features/navigation_bar/presentaions/views/navigation_bar.dart
+++ lib/features/navigation_bar/presentaions/views/navigation_bar.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/features/cart/presentation/views/cart_view.dart';
 import 'package:shopping_app/features/home/presentation/views/favorite_view.dart';
 import 'package:shopping_app/features/home/presentation/views/home_view.dart';
-import 'package:shopping_app/features/home/presentation/views/widgets/home_view/home_view_widget.dart';
 import 'package:shopping_app/features/search/presentation/views/search_view.dart';
+
 class NavigationBarForApp extends StatefulWidget {
   const NavigationBarForApp({super.key});
 
@@ -12,18 +12,19 @@ class NavigationBarForApp extends StatefulWidget {
 }
 
 class _NavigationBarState extends State<NavigationBarForApp> {
-  int selectedIndex = 0 ;
-  List<Widget> views = [
+  int selectedIndex = 0;
+  final List<Widget> _views = [
     const HomeView(),
     const SearchView(),
     const CartView(),
     const FavoriteView()
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        onTap: (index){
+        onTap: (index) {
           setState(() {
             selectedIndex = index;
           });
@@ -32,13 +33,29 @@ class _NavigationBarState extends State<NavigationBarForApp> {
         selectedItemColor: Colors.green,
         unselectedItemColor: Colors.grey,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home),label: "home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search_outlined),label: "search"),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart_rounded),label: "cart"),
-          BottomNavigationBarItem(icon: Icon(Icons.favorite_border_outlined),label: "favourite")
-        ],),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined),
+            label: "Search",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_rounded),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border_outlined),
+            label: "Favorite",
+          ),
+        ],
+      ),
       backgroundColor: const Color(0xffD6DAD8),
-      body: views.elementAt(selectedIndex)
+      body: IndexedStack(
+        index: selectedIndex,
+        children: _views,
+      ),
     );
   }
 }
