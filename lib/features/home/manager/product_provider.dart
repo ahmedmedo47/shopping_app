e18f42a2d60bce5cc1ad79645568b3c71ad64676
DELL <omarmohamed01284@gmail.com>
2024-07-26 21:21:07 +0300

    make home screen fetch one time

--- lib/features/home/manager/product_provider.dart
+++ lib/features/home/manager/product_provider.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:shopping_app/features/home/data/models/products_model/products_model.dart';
+  import 'package:flutter/material.dart';
+  import 'package:shopping_app/features/home/data/models/products_model/products_model.dart';
 
-class FavoritesProvider extends ChangeNotifier {
-  List<ProductsModel> _favorites = [];
+  class FavoritesProvider extends ChangeNotifier {
+    List<ProductsModel> _favorites = [];
 
-  List<ProductsModel> get favorites => _favorites;
+    List<ProductsModel> get favorites => _favorites;
 
-  void addFavorite(ProductsModel product) {
-    _favorites.add(product);
-    notifyListeners();
+    void addFavorite(ProductsModel product) {
+      _favorites.add(product);
+      notifyListeners();
+    }
+
+    void removeFavorite(ProductsModel product) {
+      _favorites.remove(product);
+      notifyListeners();
+    }
   }
-
-  void removeFavorite(ProductsModel product) {
-    _favorites.remove(product);
-    notifyListeners();
-  }
-}
